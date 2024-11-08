633f6656a680e6a370caa4a7ad33a0f0d8259668
Inti Pacheco <intipach@gmail.com>
2024-11-07 21:59:44 -0500

    late

---
+++ scraper.py
@@ -0,0 +1,103 @@
+import re
+import datetime
+import base64
+import os
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import (Mail, Attachment, FileContent, FileName, FileType, Disposition)
+from dotenv import load_dotenv
+
+
+
+
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+url = "https://www.thelayoff.com/nike"
+
+wait = WebDriverWait(driver, 10)
+
+driver.get(url)
+
+doc = BeautifulSoup(driver.page_source, 'html.parser')
+
+posts = doc.find_all(class_='posts')
+
+data_all = []
+for post in posts:
+    data={}
+    data['title'] = post.find(class_='post-title').text.strip()
+    try:    
+        data['body'] = re.sub(r'\n', '', post.find(class_='post-body').text.strip())
+    except:
+        pass
+    data['date'] = post.find(class_='post-timeago')['data-datetime']
+    data['link'] = 'https://www.thelayoff.com' + post.find(class_='thread-link')['href']
+    data_all.append(data)
+
+df = pd.DataFrame(data_all)
+
+df['scrape_date'] = datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
+
+try:
+    existing_df = pd.read_csv("recent_posts.csv")
+except:
+    existing_df = pd.DataFrame([])
+
+combined = pd.concat([df, existing_df], ignore_index=True)
+
+combined.drop_duplicates(subset=['link','date'], inplace=True)
+
+combined.to_csv('updated_posts.csv')
+
+#mail csv file
+load_dotenv()
+API_KEY=os.getenv('API_KEY')
+ADDRESS=os.getenv('ADDRESS')
+
+message = Mail(
+    from_email=ADDRESS,
+    to_emails=ADDRESS,
+    subject='test email api-2',
+    html_content='my head will explode')
+
+with open('updated_posts.csv', 'rb') as f:
+    data = f.read()
+    f.close()
+encoded_file = base64.b64encode(data).decode()
+
+attachedFile = Attachment(
+    FileContent(encoded_file),
+    FileName('updated_posts.csv'),
+    FileType('text/csv'),
+    Disposition('attachment')
+)
+message.attachment = attachedFile
+
+try:
+    sg = SendGridAPIClient(API_KEY)
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e)
